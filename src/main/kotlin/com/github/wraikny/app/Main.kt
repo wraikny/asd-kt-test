9d10637e219dc20f3b945a47334bf76fba2a6072
wraikny <writtenknown@gmail.com>
2018-01-29 10:05:44 +0900

    first commit

---
+++ src/main/kotlin/com/github/wraikny/app/Main.kt
@@ -0,0 +1,26 @@
+package com.github.wraikny.app
+
+fun isMac(): Boolean {
+    return System.getProperty("os.name").toLowerCase().startsWith("mac")
+}
+
+fun isWindows(): Boolean {
+    return System.getProperty("os.name").toLowerCase().startsWith("windows")
+}
+
+fun main(args: Array<String>) {
+    val path = System.getProperty("user.dir")
+    when {
+        isMac() -> System.load(path + "/libAltseed_core.dylib")
+        isWindows() -> System.load(path + "/Altseed_core.dll")
+        else -> println("Unexpected OS: " + System.getProperty("os.name"))
+    }
+
+    asd.Engine.Initialize("Test", 640, 480, asd.EngineOption())
+
+    while(asd.Engine.DoEvents()) {
+        asd.Engine.Update()
+    }
+    asd.Engine.Terminate()
+
+}
